66b6d9c9c984317ba597b594cefea6a31d04051d
Fabio Vedovelli <vedovelli@gmail.com>
2016-08-12 17:32:39 -0300

    Attempt

---
+++ src/template/single-file-component/model.vue
@@ -0,0 +1,26 @@
+
+<script>
+    export default {
+
+        props: {},
+
+        ready () {},
+
+        data () {
+            return {}
+        },
+
+        methods: {},
+
+        computed: {},
+
+    }
+</script>
+
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<style scoped></style>
